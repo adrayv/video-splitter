e31baeb02c93009de9275e76190e8fe06a7197f9
Destin Raymundo <draym001@g.ucla.edu>
2019-08-27 23:17:44 -0700

    completely removed redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from "react";
 import Splitter from "./features/Splitter";
-import { Provider } from "react-redux";
 import { Provider as UIProvider } from "./contexts/ui";
 import { Provider as VideoProvider } from "./contexts/video";
 import { Provider as ResultsProvider } from "./contexts/results";
 import { Provider as CloudinaryProvider } from "./contexts/cloudinary";
-import store from "./store";
 
 export default () => (
-  <Provider store={store}>
-    <UIProvider>
-      <VideoProvider>
-        <ResultsProvider>
-          <CloudinaryProvider>
-            <Splitter />
-          </CloudinaryProvider>
-        </ResultsProvider>
-      </VideoProvider>
-    </UIProvider>
-  </Provider>
+  <UIProvider>
+    <VideoProvider>
+      <ResultsProvider>
+        <CloudinaryProvider>
+          <Splitter />
+        </CloudinaryProvider>
+      </ResultsProvider>
+    </VideoProvider>
+  </UIProvider>
 );
